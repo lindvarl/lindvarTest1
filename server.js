6e4272608a489ae9ea59541710b56428fe54038b
Lindvar Lægran <llag@statoil.com>
2015-10-19 09:10:24 +0200

    Test

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
-
-console.log('Server started');
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
