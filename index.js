4cfef7c2cd1c38937210b59bd60a51166052e2fb
Lindvar Lægran <llag@statoil.com>
2015-10-19 11:57:36 +0200

    Test 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Load the http module to create an http server.
 var http = require('http');
 
+var port = (process.env.PORT || '3000');
+
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
@@ -8,7 +10,7 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(port);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
+console.log("Server running");
