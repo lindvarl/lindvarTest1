6de1f4126ffebd265c79bb016a486ea1d8ffbc34
Lindvar Lægran <llag@statoil.com>
2015-10-19 13:58:59 +0200

    Test 8

--- routes/store.js
+++ routes/store.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('Store');
+  res.render('store', { title: 'store' });
 });
+
+router.route('/message')
+    .post(function(req, res, next) {
+    console.log('Receiving message: ' + req.body.message);
+    res.send('Message was: ' + req.body.message);
+    });
 
 module.exports = router;
